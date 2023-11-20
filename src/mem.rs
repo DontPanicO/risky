5627fe6873452192856441010f2e85263a88ae2d
shurizzle <me@shurizzle.dev>
2023-11-20 22:50:48 +0100

    feat: optimize mem::write

--- src/mem.rs
+++ src/mem.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 
-pub unsafe trait Pod {}
+#[allow(clippy::missing_safety_doc)]
+pub unsafe trait Pod: Copy {}
 
 macro_rules! impl_pod {
     ($($t:ident($base:ty) -> $nname:ident;)*) => {
@@ -52,7 +53,7 @@ impl_pod! {
 }
 unsafe impl<T: Pod, const N: usize> Pod for [T; N] {}
 
-pub(crate) fn read<T: Pod>(src: &[u8], addr: usize) -> Result<T, Error> {
+pub fn read<T: Pod>(src: &[u8], addr: usize) -> Result<T, Error> {
     Ok(unsafe {
         core::ptr::read_unaligned(
             src.get(addr..)
@@ -64,15 +65,21 @@ pub(crate) fn read<T: Pod>(src: &[u8], addr: usize) -> Result<T, Error> {
     })
 }
 
-pub(crate) fn write<T: Pod>(src: &T, dest: &mut [u8], addr: usize) -> Result<(), Error> {
-    memw(
-        unsafe { core::slice::from_raw_parts((src as *const T).cast(), core::mem::size_of::<T>()) },
-        dest,
-        addr,
-    )
+pub fn write<T: Pod>(src: &T, dest: &mut [u8], addr: usize) -> Result<(), Error> {
+    unsafe {
+        core::ptr::write_unaligned(
+            dest.get_mut(addr..)
+                .and_then(|dst| dst.get_mut(..core::mem::size_of::<T>()))
+                .ok_or(Error::InvalidOpCode)?
+                .as_mut_ptr()
+                .cast(),
+            *src,
+        )
+    };
+    Ok(())
 }
 
-pub(crate) fn memw(src: &[u8], dest: &mut [u8], addr: usize) -> Result<(), Error> {
+pub fn memw(src: &[u8], dest: &mut [u8], addr: usize) -> Result<(), Error> {
     let dest = dest
         .get_mut(addr..)
         .and_then(|src| src.get_mut(..src.len()))
@@ -82,23 +89,29 @@ pub(crate) fn memw(src: &[u8], dest: &mut [u8], addr: usize) -> Result<(), Error
     Ok(())
 }
 
-pub(crate) fn memr<const N: usize>(src: &[u8], addr: usize) -> Result<[u8; N], Error> {
-    read::<[u8; N]>(src, addr)
+pub fn memr<const N: usize>(dest: &mut [u8], src: &[u8], addr: usize) -> Result<(), Error> {
+    let src = src
+        .get(addr..)
+        .and_then(|src| src.get(..dest.len()))
+        .ok_or(Error::InvalidOpCode)?
+        .as_ptr();
+    unsafe { core::ptr::copy_nonoverlapping(src, dest.as_mut_ptr(), dest.len()) };
+    Ok(())
 }
 
 #[inline(always)]
-pub(crate) fn memr32(src: &[u8], addr: usize) -> Result<[u8; 4], Error> {
-    memr::<4>(src, addr)
+pub fn memr32(src: &[u8], addr: usize) -> Result<[u8; 4], Error> {
+    read::<[u8; 4]>(src, addr)
 }
 
 #[inline(always)]
-pub(crate) fn memr16(src: &[u8], addr: usize) -> Result<[u8; 2], Error> {
-    memr::<2>(src, addr)
+pub fn memr16(src: &[u8], addr: usize) -> Result<[u8; 2], Error> {
+    read::<[u8; 2]>(src, addr)
 }
 
 #[inline(always)]
-pub(crate) fn memr8(src: &[u8], addr: usize) -> Result<u8, Error> {
-    memr::<1>(src, addr).map(|n| unsafe { core::mem::transmute(n) })
+pub fn memr8(src: &[u8], addr: usize) -> Result<u8, Error> {
+    read::<[u8; 1]>(src, addr).map(|n| unsafe { core::mem::transmute(n) })
 }
 
 #[cfg(test)]
